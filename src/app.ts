ced95ef784f2d698460fc6777bfcb0ad46d332d3
Aladdin Al-Wattar <aladdin83@gmail.com>
2019-03-25 13:36:53 +0400

    added socket io

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import * as express from "express";
 import * as mongoose from "mongoose";
 import * as http from "http";
 import { CronJob } from "cron";
+import * as socketio from "socket.io";
 
 import registerRoutes from "./routes";
 import expressConfig from "./config/express";
-import config from "./config/enviroment";
+import config from "./config/environment/";
 import { HekaConfig } from "./@heka/core/config";
 import { MongoError } from "mongodb";
+import { registerSockets } from "./socketio";
 
 class App {
   express: express.Application;
@@ -15,22 +17,39 @@ class App {
   db: mongoose.Connection;
   server: http.Server;
   cronJobs: any;
+  socketioServer: SocketIO.Server;
   constructor() {
     this.config = config;
     this.express = express();
     this.server = http.createServer(this.express as any);
-    this.initDatabaseConnection();
-
-    expressConfig(this.express, this.config);
-    registerRoutes(this.express);
-    this.setupRssSyncService()
-  }
-
-  initDatabaseConnection() {
-    mongoose.connect(this.config.mongo.uri, {useNewUrlParser: true}, (err: MongoError) => {});
     this.db = mongoose.connection;
     this.db.on("error", this.dbConnectionError);
     this.db.on("open", this.dbConnectionSuccess);
+  }
+
+  init(){
+    expressConfig(this.express, this.config);
+    registerRoutes(this.express);
+    this.socketioServer = socketio(this.server);
+    registerSockets(this.socketioServer);
+    this.connectDatabase();
+    this.startAllCronJobs();
+  }
+
+  connectDatabase(this: App) {
+    const options = {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    };
+    if (!this.config && this.config.mongo && this.config.mongo.uri) throw new Error("database uri is not set");
+    const mongoUri = this.config.mongo.uri || "";
+    mongoose.connect(mongoUri, options, (err: MongoError) => {
+      if (err) {this.dbConnectionError(); }
+      else {
+        this.dbConnectionSuccess();
+      }
+    });
   }
 
   dbConnectionError() {
@@ -41,9 +60,10 @@ class App {
 
   }
 
-  
-  setupRssSyncService() {
-    this.cronJobs["RssService"] = new CronJob()
+  startAllCronJobs() {
+    for (const key in this.cronJobs) {
+      (this.cronJobs[key] as CronJob).start();
+    }
   }
 }
 
---
+++ src/socketio.ts
@@ -0,0 +1,2 @@
+
+export function registerSockets(socketio: SocketIO.Server) {}
